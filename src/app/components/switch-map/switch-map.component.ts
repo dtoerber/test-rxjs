5a3ea6bd1243b71b7b6ebbfced1ea6ed599c93db
Dave Toerber <c-david.toerber@charter.com>
2020-11-17 10:04:38 -0700

    add some observables

--- src/app/components/switch-map/switch-map.component.ts
+++ src/app/components/switch-map/switch-map.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable, from, of } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 @Component({
   selector: 'app-switch-map',
   templateUrl: './switch-map.component.html',
-  styleUrls: ['./switch-map.component.scss']
+  styleUrls: ['./switch-map.component.scss'],
 })
 export class SwitchMapComponent implements OnInit {
+  outer$: Observable<number[]> = of([0, 1, 2, 3, 4]).pipe(
+    tap((t) => console.log(`in an iteration val:`, t))
+  );
+  inner$: Observable<number[]> = of([0, 1, 2, 3, 4]);
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
