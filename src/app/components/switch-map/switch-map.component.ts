36ef24a6da0c82a8f7f071279fccf316fae78375
Dave Toerber <c-david.toerber@charter.com>
2020-11-17 12:35:40 -0700

    try throttle

--- src/app/components/switch-map/switch-map.component.ts
+++ src/app/components/switch-map/switch-map.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, from, of } from 'rxjs';
-import { switchMap, tap } from 'rxjs/operators';
+import { Observable, from, of, interval } from 'rxjs';
+import { switchMap, tap, take, map, exhaustMap, delay } from 'rxjs/operators';
 @Component({
   selector: 'app-switch-map',
   templateUrl: './switch-map.component.html',
   styleUrls: ['./switch-map.component.scss'],
 })
 export class SwitchMapComponent implements OnInit {
-  outer$: Observable<number[]> = of([0, 1, 2, 3, 4]).pipe(
-    tap((t) => console.log(`in an iteration val:`, t))
+  letter$: Observable<string> = interval(500).pipe(
+    map((x) => ['A', 'B', 'C'][x])
   );
-  inner$: Observable<number[]> = of([0, 1, 2, 3, 4]);
+
+  outer$: Observable<any> = interval(500).pipe(
+    take(5),
+    map((d) => `outer ${d}`)
+  );
+
+  inner$: Observable<any> = interval(500).pipe(
+    take(5),
+    map((d) => `inner ${d}`)
+  );
+
+  // test$: Observable<any> = this.outer$.pipe(switchMap((o) => this.inner$));
+  test$: Observable<any> = this.inner$.pipe(switchMap((o) => this.outer$));
+
   constructor() {}
 
   ngOnInit(): void {}
